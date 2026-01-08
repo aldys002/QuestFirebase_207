b376c9063664dd831ae4dacb7a66ce3c6c968965
aldys002 <igidiaaldys@gmail.com>
2026-01-08 13:26:59 +0700

    import navtype

--- app/src/main/java/com/example/firebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/firebase/view/controllNavigasi/PetaNavigasi.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
